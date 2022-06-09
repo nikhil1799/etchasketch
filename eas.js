b4d792415d83140229c1e79cede889a7c4e6c3f8
nikhil1799 <78519027+nikhil1799@users.noreply.github.com>
2022-06-09 16:06:18 -0400

    test divs

---
+++ eas.js
@@ -0,0 +1,21 @@
+
+    function genDivs(v){ 
+      var e = document.body; // whatever you want to append the rows to: 
+      for(var i = 0; i < v; i++){ 
+        var row = document.createElement("div"); 
+        row.className = "row"; 
+        for(var x = 1; x <= v; x++){ 
+            var cell = document.createElement("div"); 
+            cell.className = "gridsquare"; 
+            cell.innerText = (i * v) + x;
+            
+            row.appendChild(cell); 
+            document.getElementById("gridsquare").style.border = "thick solid #0000FF";
+        } 
+        e.appendChild(row); 
+        document.getElementById("gridsquare").style.border = "thick solid #0000FF";
+      } 
+      document.getElementById("code").innerText = e.innerHTML;
+
+    }
+ 
